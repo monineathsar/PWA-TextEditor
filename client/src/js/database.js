1ba786c6f60ad3fd7be9a7faa53c3bea431cf206
monineathsar <monineathsar@gmail.com>
2022-09-30 16:49:13 -0700

    Added database logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,7 @@
 import { openDB } from 'idb';
 
+const jateDatabaseId = 1;
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +15,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const storeObject = establishStoreObject('readwrite');
+  const request = storeObject.put({id: jateDatabaseId, value: content});
+  const result = await request;
+  console.log('Data is saved to database! ', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const storeObject = establishStoreObject('readonly');
+  const request = storeObject.get(jateDatabaseId);
+  const result = await(request);
+  return result?.value;
+};
+
+async function establishStoreObject(type) {
+  const database = await openDB('jate', 1);
+  const transcation = database.transaction('jate', type);
+  const storeObject = transcation.objectStore('jate');
+  return storeObject;
+}
 
 initdb();
