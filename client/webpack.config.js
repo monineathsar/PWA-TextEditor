1c986f2f4f49ec4626c0d24164b6b3a66a426913
monineathsar <monineathsar@gmail.com>
2022-09-30 18:00:26 -0700

    Removed typo and reformat the script

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,73 +1,72 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
       new HtmlWebpackPlugin({
-				template: './index.html',
-        title: 'JATE'
-			}),
+        template: "./index.html",
+        title: "JATE",
+      }),
       // TODO: Add and configure workbox plugins for a service worker and manifest file.
       new InjectManifest({
-				swSrc: './src-sw.js',
-				swDest: 'service-worker.js',
-			}),
-			new WebpackPwaManifest({
-				name: 'JATE',
-				short_name: 'JATE',
-				description: 'Just Another Text Editor',
-				display: 'standalone',
-				background_color: '#225ca3',
-				theme_color: '#225ca3',
-				start_url: '/',
-				publicPath: '/',
-				fingerprints: false,
-				inject: true,
-				icons: [
-					{
-						src: path.resolve('src/images/logo.png'),
-						sizes: [96, 128, 192, 256, 384, 512],
-						destination: path.join('assets', 'icons'),
-					}
-				]
-			})
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "JATE",
+        short_name: "JATE",
+        description: "Just Another Text Editor",
+        display: "standalone",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
-				{
-					test: /\.css$/i,
-					use: ['style-loader', 'css-loader'],
-				},
-				{
-					test: /\.m?js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env'],
-							plugins: [
-								'@babel/plugin-proposal-object-rest-spread',
-								'@babel/transform-runtime',
-							]
-						}
-					}
-				}
-			]
-    }
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
   };
 };
